e37664240920dd54c8762140a29da4a0c426c50e
Stephen <stephenafoster88@gmail.com>
2024-02-17 14:00:39 +0000

    Add components for KeyboardExtended and MouseExtended state updates. Add extension method for adding both components to the collection.

---
+++ src/cs/MonoGame.Extended.Input/Components/KeyboardExtendedComponent.cs
@@ -0,0 +1,15 @@
+﻿namespace MonoGame.Extended.Input.Components;
+
+using Microsoft.Xna.Framework;
+
+/// <summary>
+/// Will update <see cref="KeyboardExtended"/> states when added to the GameComponents list
+/// </summary>
+public class KeyboardExtendedComponent : SimpleGameComponent
+{
+    /// <summary>
+    /// Will update <see cref="KeyboardExtended"/> states when added to the GameComponents list
+    /// </summary>
+    /// <param name="gameTime"></param>
+    public override void Update(GameTime gameTime) => KeyboardExtended.Refresh();
+}
